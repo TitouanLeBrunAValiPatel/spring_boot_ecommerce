0f192cf802318ba25bf7e9d7c07d7b09cf46467a
Titouan <titouan3.lebrunavalipatel@gmail.com>
2024-03-22 14:16:34 +0100

    first commit final

--- src/main/java/com/ecommerce/ecommercebackend/controllers/ProductController.java
+++ src/main/java/com/ecommerce/ecommercebackend/controllers/ProductController.java
@@ -1,13 +1,8 @@
 package com.ecommerce.ecommercebackend.controllers;
 
-import com.ecommerce.ecommercebackend.dtos.ProductCreate;
 import com.ecommerce.ecommercebackend.entities.Product;
-import com.ecommerce.ecommercebackend.exceptions.CategoryException;
 import com.ecommerce.ecommercebackend.exceptions.ProductException;
 import com.ecommerce.ecommercebackend.services.ProductService;
-import jakarta.validation.Valid;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -32,14 +27,10 @@ public class ProductController {
         return this.productService.getProducts();
     }
 
+
     @GetMapping(path = "/{id}")
-    public ResponseEntity<Product> getProduct(@PathVariable(name = "id") int productId) throws ProductException {
-        return ResponseEntity.ok(this.productService.getProduct(productId));
+    public Product getProduct(@PathVariable(name = "id") int productId) throws ProductException {
+        return this.productService.getProduct(productId);
     }
 
-
-    @PostMapping("/new")
-    public ResponseEntity<Product> createProduct(@RequestBody @Valid ProductCreate product) throws CategoryException, ProductException {
-        return new ResponseEntity<>(this.productService.createProduct(product), HttpStatus.CREATED);
-    }
 }
--- src/main/java/com/ecommerce/ecommercebackend/services/ProductService.java
+++ src/main/java/com/ecommerce/ecommercebackend/services/ProductService.java
@@ -1,9 +1,6 @@
 package com.ecommerce.ecommercebackend.services;
 
-import com.ecommerce.ecommercebackend.dtos.ProductCreate;
-import com.ecommerce.ecommercebackend.entities.Category;
 import com.ecommerce.ecommercebackend.entities.Product;
-import com.ecommerce.ecommercebackend.exceptions.CategoryException;
 import com.ecommerce.ecommercebackend.exceptions.ProductException;
 import com.ecommerce.ecommercebackend.repositories.IProductRepository;
 import org.springframework.stereotype.Service;
@@ -36,27 +33,5 @@ public class ProductService {
             throw new ProductException("Product Not found");
         }
 
-    }
-
-    public Product createProduct(ProductCreate productCreate) throws CategoryException, ProductException {
-        Product newProduct = convertToProduct(productCreate);
-        newProduct = this.iProductRepository.save(newProduct);
-        return newProduct;
-
-    }
-
-    private Product convertToProduct(ProductCreate productCreate) throws CategoryException {
-        Product newProduct = new Product();
-
-        int idCategory = productCreate.getIdCategory();
-        Category category = categoryService.getCategory(idCategory);
-        newProduct.setCategory(category);
-
-        newProduct.setName(productCreate.getName());
-        newProduct.setDescription(productCreate.getDescription());
-        newProduct.setStock(productCreate.getStock());
-        newProduct.setPrice(productCreate.getPrice());
-
-        return newProduct;
     }
 }
